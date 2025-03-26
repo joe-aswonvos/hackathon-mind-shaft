0de9b5c86ec03df826db7a42b05b9ae038388edf
Joe Ashton <joe@aswonvos.com>
2025-03-26 16:41:48 +0000

    Initialize Django project and configure environment settings
    Set up the "MindShaft" Django project with base settings, templates, and essential configuration files. Added support for environment variables, Heroku deployment, and dynamic database connections. Updated dependencies, README, and metadata files to complete the project initialization.

--- MindShaft/settings.py
+++ MindShaft/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import sys
 from decouple import config
 import dj_database_url
 
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn MindShaft.wsgi
