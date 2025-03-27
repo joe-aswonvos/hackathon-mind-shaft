8a89c23abbc20cff92204ee8a65d9b0650a3ecd9
Joe Ashton <joe@aswonvos.com>
2025-03-27 12:00:06 +0000

    Add URL routing and basic views for flash_cards app
    Defined URLs and corresponding views for flash_cards, including deck and card management actions. Updated the main project to include flash_cards URLs as the root path and added templates to render views.

--- flash_cards/views.py
+++ flash_cards/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request, "flash_cards/index.html")
+
+def create_deck(request):
+    return render(request, "flash_cards/create_edit_deck.html")
+
+def deck(request, deck_id):
+    return render(request, "flash_cards/deck.html")
+
+def edit_deck(request, deck_id):
+    return render(request, "flash_cards/create_edit_deck.html")
+
+def delete_deck(request, deck_id):
+    return render(request, "flash_cards/index.html")
+
+def add_card(request, deck_id):
+    return render(request, "flash_cards/create_edit_card.html")
+
+def edit_card(request, deck_id, card_id):
+    return render(request, "flash_cards/create_edit_card.html")
+
+def delete_card(request, deck_id, card_id):
+    return render(request, "flash_cards/deck.html")
