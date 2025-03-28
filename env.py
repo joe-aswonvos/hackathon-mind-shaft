179e3fbcd8174ef41eff43dfd0b2bd53adffcf87
Joe Ashton <joe@aswonvos.com>
2025-03-28 13:15:05 +0000

    Update repo

--- env.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.environ['SECRET_KEY'] = 'thq$&d_ka5ew7!c%@zj8y@(!)-t#8=9c=+39t$h7w*1d@tm()u'
-os.environ['DEBUG'] = 'True'
-os.environ['DATABASE_URL'] = 'postgresql://neondb_owner:npg_HAn25yRqDSiF@ep-hidden-frost-a2s44uyw.eu-central-1.aws.neon.tech/photo_props_line_546750'
